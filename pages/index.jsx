cd2be6a96d1141221c81a4b044a816d8f6f7d748
uwang20 <ohjuan205@gmail.com>
2022-04-08 16:04:56 +0800

    yow

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,34 +1,67 @@
 import Head from 'next/head'
-import { useSelector, useDispatch } from 'react-redux'
-import {increment, decrement, incrementByAmount, fetchPokemon} from '../store/counterSlice'
+import {useRouter} from 'next/router'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
-import {useState} from 'react'
 
 export default function Home() {
-  const {count, pokemons, loading} = useSelector(state => state.counter)
-  const dispatch = useDispatch()
-  const [incByAmount, setIncByAmount] = useState(0)
+  const appComponents = [
+    {
+        id:0,
+        name: 'calculator'
+    },
+    {
+        id:1,
+        name: 'todo'
+    }
+]
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>{count}</h1>
-      <button onClick={() => dispatch(fetchPokemon(1))}>FETCH POKEMON</button>
       <ul>
         {
-          loading && <li>LOADING</li>
-        }
-        {
-          pokemons &&
-          pokemons.map(p => {
+          appComponents.map(app => {
             return(
-              <li key={p.id}>{p.name}</li>
+              <li key={app.id} className="link">
+                <Link  href={`/${app.name}`}>
+                  <a>
+                    {app.name}
+                  </a>
+                </Link>
+              </li>
             )
           })
         }
       </ul>
+      <style jsx>
+        {`
+        
+          ul {
+            list-style: none;
+          }
+
+          .link {
+            background-color: skyblue;
+            padding: 1rem 3rem;
+            text-transform: uppercase;
+            font-weight: 900;
+            font-size: 2rem;
+            color: white;
+            border-radius: 1rem;
+            margin: 0 0 1rem 0;
+            text-align: center;
+            transition: all 1s linear;
+          }
+
+          .link:hover {
+            cursor: pointer;
+            filter: contrast(1.5);
+          }
+        
+        `}
+      </style>
     </div>
   )
 }
